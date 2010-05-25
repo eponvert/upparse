502ca432897a979d55e66f57b8f599ee5e221e48
Elias Ponvert <ponvert@gmail.com>
2010-05-24 23:59:18 -0500

    script ready to go

--- bin/upparse.sh
+++ bin/upparse.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ -z $JAVA_MEM_FLAG ] ; then
-  JAVA_MEM_FLAG=-Xmx125m
-fi
+if [ -z $JAVA_MEM_FLAG ] ; then JAVA_MEM_FLAG=-Xmx125m ; fi
 
-echo $JAVA_MEM_FLAG
-
-# java $JAVA_MEM_FLAG -cp classes -ea upparse.Main $@
+java $JAVA_MEM_FLAG -cp classes -ea upparse.Main $@
