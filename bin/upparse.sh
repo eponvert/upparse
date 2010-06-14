1588e111f70d3510ac5d35bee942f302280a9f52
Elias Ponvert <ponvert@gmail.com>
2010-06-14 15:55:23 -0500

    simplified bin/upparse -- dont need so much mem

--- bin/upparse.sh
+++ bin/upparse.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-
-if [ -z $JAVA_MEM_FLAG ] ; then JAVA_MEM_FLAG="-Xmx512M -Xms512M" ; fi
-
-java $JAVA_MEM_FLAG -cp classes -ea upparse.Main $@
+java -Xmx512M -Xms512M -cp classes -ea upparse.Main $@
