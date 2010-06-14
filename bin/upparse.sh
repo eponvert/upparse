444694ef1473578a8c7f8b6fad7cec42af2cf832
Elias Ponvert <ponvert@gmail.com>
2010-06-14 17:02:05 -0500

    reverting bin/upparse to allow more memory config

--- bin/upparse.sh
+++ bin/upparse.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-java -Xmx512M -Xms512M -cp classes -ea upparse.Main $@
+
+if [ -z $JAVA_MEM_FLAG ] ; then JAVA_MEM_FLAG="-Xmx512M -Xms512M" ; fi
+
+java $JAVA_MEM_FLAG -cp classes -ea upparse.Main $@
